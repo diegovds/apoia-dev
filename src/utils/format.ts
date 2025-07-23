4e08b66236d307e3f1ac79d2885076c496561ee0
Diego Viana <dsantos@inf.ufsm.br>
2025-07-22 22:05:14 -0300

    Add currency and date formatting utilities
    Introduces formatCurrency and formatDate functions for formatting numbers as BRL currency and dates in 'pt-BR' locale, respectively.

---
+++ src/utils/format.ts
@@ -0,0 +1,14 @@
+export function formatCurrency(value: number): string {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value / 100)
+}
+
+export function formatDate(date: Date): string {
+  return new Intl.DateTimeFormat('pt-BR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(new Date(date))
+}
